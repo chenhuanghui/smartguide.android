70c4491bcb6e37b629f48b5e5d458a92d17cc067
nncsang <ng.ng.ch.sang@gmail.com>
2013-08-30 12:29:40 +0700

    fix flash screen

--- SmartGuide/src/vn/smartguide/FlashScreenActivity.java
+++ SmartGuide/src/vn/smartguide/FlashScreenActivity.java
@@ -40,22 +40,8 @@ import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.ImageView;
 
 public class FlashScreenActivity extends Activity {
-
-	ImageView mLogo = null;
-	ImageView mSlogan = null;
-	ImageView mSmartGuide = null;
-
-	int mHeight = 0;
-	int mWidth = 0;
-	int marginTop = 40;
-
-	private ObjectAnimator mSloganSlideUp = null;
-	private ObjectAnimator mLogoFadeIn = null;
-	private ObjectAnimator mLogoSlideUp = null;
-	private ObjectAnimator mSmartGuideSlideUp = null;
-
 	private boolean isShow = false;
-	private boolean isFinish = false;
+	
 	Intent resultData = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -83,151 +69,6 @@ public class FlashScreenActivity extends Activity {
 		// Initiate lots of things
 		new InitInformation().execute();
 
-		// Set up animation
-//		mLogo = (ImageView)findViewById(R.id.logoF);
-//		mSlogan = (ImageView)findViewById(R.id.sloganF);
-//		mSmartGuide = (ImageView)findViewById(R.id.smartguideF);
-//
-//		Display display = getWindowManager().getDefaultDisplay(); 
-//		mWidth = display.getWidth();
-//		mHeight = display.getHeight();
-//
-//		int halfWidth = mWidth / 2;
-//
-//		mLogo.setX(halfWidth - mLogo.getWidth() / 2);
-//		mSlogan.setX(halfWidth - mSlogan.getWidth() / 2);
-//		mSmartGuide.setX(halfWidth - mSmartGuide.getWidth() / 2);
-//
-//		mLogoFadeIn = ObjectAnimator.ofFloat(mLogo, "alpha", 0.0f, 1.0f);
-//		mLogoFadeIn.setInterpolator(new AccelerateDecelerateInterpolator());
-//
-//		mLogoSlideUp = ObjectAnimator.ofFloat(mLogo, "translationY", mHeight, mHeight / 2 - 100 - 95);
-//		mLogoSlideUp.setInterpolator(new AccelerateDecelerateInterpolator());
-//		mLogoSlideUp.addListener(new AnimatorListener() {
-//			@Override
-//			public void onAnimationStart(Animator animation) {
-//				mLogo.setVisibility(View.VISIBLE);
-//				new Handler().postDelayed(new Runnable() {
-//
-//					@Override
-//					public void run() {
-//						mSmartGuide.setVisibility(View.VISIBLE);
-//						mSmartGuideSlideUp.start();
-//					}
-//				}, 500);
-//			}
-//
-//			@Override
-//			public void onAnimationRepeat(Animator animation) {
-//
-//			}
-//
-//			@Override
-//			public void onAnimationEnd(Animator animation) {
-//
-//			}
-//
-//			@Override
-//			public void onAnimationCancel(Animator animation) {
-//
-//			}
-//		});
-//
-//		List<ObjectAnimator> ListLogoAnimators = new ArrayList<ObjectAnimator>();
-//		ListLogoAnimators.add(mLogoFadeIn);
-//		ListLogoAnimators.add(mLogoSlideUp);
-//
-//		ObjectAnimator[] objectAnimators = ListLogoAnimators.toArray(new ObjectAnimator[ListLogoAnimators.size()]);
-//		AnimatorSet animSetXY = new AnimatorSet();
-//		animSetXY.playTogether(objectAnimators);
-//		animSetXY.setDuration(1000);//1sec
-//		animSetXY.start();
-//
-//		mSmartGuideSlideUp = ObjectAnimator.ofFloat(mSmartGuide, "translationY", mHeight, mHeight / 2 + 10);
-//		mSmartGuideSlideUp.setDuration(1000);
-//		mSmartGuideSlideUp.setInterpolator(new AccelerateDecelerateInterpolator());
-//		mSmartGuideSlideUp.addListener(new AnimatorListener() {
-//			@Override
-//			public void onAnimationStart(Animator animation) {
-//				// TODO Auto-generated method stub
-//				new Handler().postDelayed(new Runnable() {
-//
-//					@Override
-//					public void run() {
-//						mSlogan.setVisibility(View.VISIBLE);
-//						mSloganSlideUp.start();
-//					}
-//				}, 500);
-//			}
-//
-//			@Override
-//			public void onAnimationRepeat(Animator animation) {}
-//
-//			@Override
-//			public void onAnimationEnd(Animator animation) {}
-//
-//			@Override
-//			public void onAnimationCancel(Animator animation) {}
-//		});
-//		//mSmartGuideSlideUp.start();
-//
-//		mSloganSlideUp = ObjectAnimator.ofFloat(mSlogan, "translationY", mHeight, mHeight / 2 + 65);
-//		mSloganSlideUp.setDuration(1000);
-//		mSloganSlideUp.setInterpolator(new AccelerateDecelerateInterpolator());
-//		mSloganSlideUp.addListener(new AnimatorListener() {
-//
-//			@Override
-//			public void onAnimationStart(Animator animation) {
-//
-//			}
-//
-//			@Override
-//			public void onAnimationRepeat(Animator animation) {
-//
-//			}
-//
-//			@Override
-//			public void onAnimationEnd(Animator animation) {
-//				new Handler().postDelayed(new Runnable() {
-//
-//					@Override
-//					public void run() {
-//						if (isFinish){
-//
-//							if (getParent() == null) 
-//								setResult(Activity.RESULT_OK, resultData);
-//							else
-//								getParent().setResult(Activity.RESULT_OK, resultData);
-//
-//							finish();
-//							//overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
-//							overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
-//						}
-//
-//						isFinish = true;
-//					}
-//				}, 200);
-//			}
-//
-//			@Override
-//			public void onAnimationCancel(Animator animation) {
-//
-//			}
-//		});
-		
-		// Finish this activity after 2000 ms
-		new Handler().postDelayed(new Runnable() {
-			
-			@Override
-			public void run() {
-				
-				if (isFinish) {
-					finish();
-					overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
-				}
-				isFinish = true;
-			}
-		}, 2000);
 	}
 
 	public class InitInformation extends AsyncTask<Void, Void, Boolean> {
@@ -266,7 +107,7 @@ public class FlashScreenActivity extends Activity {
 			.imageScaleType(ImageScaleType.EXACTLY)
 			.displayer(new RoundedBitmapDisplayer(20))
 			.build();
-			
+
 			ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
 			.threadPoolSize(6)
 			.threadPriority(Thread.NORM_PRIORITY-1)
@@ -282,9 +123,9 @@ public class FlashScreenActivity extends Activity {
 			List<NameValuePair> pairs = new ArrayList<NameValuePair>();
 			pairs.add(new BasicNameValuePair("city", GlobalVariable.mCityID));
 			pairs.add(new BasicNameValuePair("env", Integer.toString(GlobalVariable.mMode)));
-			
+
 			json = NetworkManger.post(APILinkMaker.mGroupByCity(), pairs);
-			
+
 			if (json != ""){
 				resultData = new Intent();
 				resultData.putExtra("Database", "OK");
@@ -302,7 +143,7 @@ public class FlashScreenActivity extends Activity {
 						GlobalVariable.mCateogries = Category.getListCategory(object.getJSONArray("content"));
 						pairs = new ArrayList<NameValuePair>();
 						pairs.add(new BasicNameValuePair("type", "1")); // lấy version của city
-						
+
 						json = NetworkManger.post(APILinkMaker.mGetVersion(), pairs);
 						String version = json.substring(1, json.length() - 1);
 						if (version.compareTo(GlobalVariable.mVersion) != 0){
@@ -331,19 +172,15 @@ public class FlashScreenActivity extends Activity {
 					setResult(Activity.RESULT_OK, resultData);
 				else
 					getParent().setResult(Activity.RESULT_OK, resultData);
-
-				if (isFinish == true){
-					finish();
-					overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
-				}
-
-				isFinish = true;
+				
+				finish();
+				overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
 			}
 		}
 
 		@Override
 		protected void onPreExecute(){
-			
+
 		}
 	}
 
@@ -351,7 +188,7 @@ public class FlashScreenActivity extends Activity {
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
 	}
-	
+
 	@Override
 	public void onStart() {
 		super.onStart();
@@ -363,7 +200,7 @@ public class FlashScreenActivity extends Activity {
 		super.onStop();
 		EasyTracker.getInstance(this).activityStop(this);
 	}
-	
+
 	@Override
 	public void onBackPressed() {
 		return;
