4a0723e73086e96d4bb62584673c17d8cab9b52d
Gia Hung Thai <hung.tg89@gmail.com>
2014-08-04 21:59:07 +0700

    Thêm sự kiện click vào header

--- Infory/src/vn/infory/infory/home/HomeItemUpdater_Header.java
+++ Infory/src/vn/infory/infory/home/HomeItemUpdater_Header.java
@@ -1,27 +1,99 @@
 package vn.infory.infory.home;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import vn.infory.infory.R;
+import vn.infory.infory.data.PlaceList;
+import vn.infory.infory.data.Shop;
 import vn.infory.infory.data.home.HomeItem;
 import vn.infory.infory.data.home.HomeItem_Header;
+import vn.infory.infory.network.CyAsyncTask;
+import vn.infory.infory.network.GetPlaceListDetail;
+import vn.infory.infory.network.NetworkManager;
+import vn.infory.infory.shoplist.ShopListActivity;
+import android.graphics.drawable.AnimationDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomeItemUpdater_Header extends HomeItemUpdater{
 
 	private LayoutInflater mInflater;
+	private List<CyAsyncTask> mTaskList = new ArrayList<CyAsyncTask>();
 	
 	@Override
-	public void update(View view, HomeItem item, HomeFragment caller) {
+	public void update(View view, HomeItem item, final HomeFragment caller) {
 		// TODO Auto-generated method stub
 		mInflater = caller.getActivity().getLayoutInflater();
 		
-		HomeItem_Header itemHeader = (HomeItem_Header) item;
+		final HomeItem_Header itemHeader = (HomeItem_Header) item;
 		
 		TextView txtHeaderContent = (TextView) view.findViewById(R.id.txtHeaderContent);
+		RelativeLayout relativeLayoutHeader = (RelativeLayout) view.findViewById(R.id.relativeLayoutHeader);
+		final FrameLayout mLayoutLoading = (FrameLayout) view.findViewById(R.id.layoutLoading);
+		final FrameLayout mLayoutLoadingAni = (FrameLayout) view.findViewById(R.id.layoutLoadingAni);
 		
-		txtHeaderContent.setText(itemHeader.title);		
+		txtHeaderContent.setText(itemHeader.title);	
+		
+		relativeLayoutHeader.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if(itemHeader.idPlacelist != 0)
+				{
+					/*mLayoutLoading.setVisibility(View.VISIBLE);
+					
+		    		AnimationDrawable frameAnimation = (AnimationDrawable) 
+		    				mLayoutLoadingAni.getBackground();
+		    		frameAnimation.start();*/
+		    		
+					try {
+						int id_placelist = itemHeader.idPlacelist;
+						
+						GetPlaceListDetail place_list_task = new GetPlaceListDetail(caller.getActivity(), id_placelist, 0){
+
+							@Override
+							protected void onCompleted(Object result) throws Exception {
+								// TODO Auto-generated method stub
+								mTaskList.remove(this);
+								
+								Object[] placelist = (Object[]) result;
+								ShopListActivity.newInstance(caller.getActivity(), (PlaceList)placelist[0], new ArrayList<Shop>());
+							}
+
+							@Override
+							protected void onFail(
+									Exception e) {
+								mTaskList.remove(this);
+							}														
+						};	
+						
+						mTaskList.add(place_list_task);
+						place_list_task.executeOnExecutor(NetworkManager.THREAD_POOL);
+					} catch (Exception e) {
+						// TODO: handle exception
+					}				
+				}
+				else if(itemHeader.idShops != "")
+				{
+					/*mLayoutLoading.setVisibility(View.VISIBLE);
+					
+		    		AnimationDrawable frameAnimation = (AnimationDrawable) 
+		    				mLayoutLoadingAni.getBackground();
+		    		frameAnimation.start();*/
+		    		
+					String id_shops = itemHeader.idShops;
+					ShopListActivity.newInstance(caller.getActivity(), id_shops, new ArrayList<Shop>(),0);
+				}
+			}
+		});
+		
 	}
 
 }
